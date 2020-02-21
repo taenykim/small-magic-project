dcc33cbac4d3e5f6fc86f146969d2b84da312a9c
taeeun kim <kimtaeeeny@gmail.com>
2020-02-21 16:08:05 +0900

    :apple: calculator plus button

--- components/calculator/Layout.js
+++ components/calculator/Layout.js
@@ -11,21 +11,38 @@ const CalculatorContainer = styled.div`
 `
 const Layout = () => {
   const [result, setResult] = useState('')
+  const [tempResult, setTempResult] = useState('')
+  const [isOperatorPressed, setIsOperatorPressed] = useState(false)
 
   const buttonClickHandler = button_type => {
-    console.log(button_type)
+    console.log(tempResult)
     if (button_type === 'reset') {
       setResult('')
+      setTempResult('')
     } else if (button_type === 'delete') {
       setResult(result.substr(0, result.length - 1))
+    } else if (button_type === 'plus') {
+      const block_result = String(Number(tempResult) + Number(result))
+      if (tempResult !== '') {
+        setResult(block_result)
+      }
+      setTempResult(block_result)
+      setIsOperatorPressed(true)
+    } else if (button_type === 'minus') {
+      setResult()
     } else if (button_type === '1' || '2' || '3' || '4' || '5' || '6' || '7' || '8' || '9' || '0') {
-      setResult(result + button_type)
+      if (isOperatorPressed) {
+        setResult(button_type)
+        setIsOperatorPressed(false)
+      } else {
+        setResult(result + button_type)
+      }
     }
   }
 
   return (
     <CalculatorContainer>
-      <div style={{ marginTop: '20px' }}>{result}</div>
+      <div style={{ marginTop: '20px', height: '40px' }}>{result}</div>
       <button name="1" type="button" onClick={e => buttonClickHandler(e.target.name)}>
         1
       </button>
@@ -61,6 +78,12 @@ const Layout = () => {
       </button>
       <button name="delete" type="button" onClick={e => buttonClickHandler(e.target.name)}>
         delete
+      </button>
+      <button name="plus" type="button" onClick={e => buttonClickHandler(e.target.name)}>
+        plus
+      </button>
+      <button name="minus" type="button" onClick={e => buttonClickHandler(e.target.name)}>
+        minus
       </button>
     </CalculatorContainer>
   )
