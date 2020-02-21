2b4ea854f01eddb16658a4d60d2ff196e985b382
taeeun kim <kimtaeeeny@gmail.com>
2020-02-21 14:48:17 +0900

    add calculator number button (refactoring not yet)

--- components/calculator/Layout.js
+++ components/calculator/Layout.js
@@ -12,15 +12,32 @@ const CalculatorContainer = styled.div`
 const Layout = () => {
   const [number, setNumber] = useState(0)
 
-  const buttonClickHandler = () => {
-    setNumber(number + 1)
+  const buttonClickHandler = button_type => {
+    if (button_type === '1') {
+      setNumber(1)
+    } else if (button_type === '2') {
+      setNumber(2)
+    } else if (button_type === '3') {
+      setNumber(3)
+    } else if (button_type === '4') {
+      setNumber(4)
+    }
   }
 
   return (
     <CalculatorContainer>
       <div style={{ marginTop: '20px' }}>{number}</div>
-      <button type="button" onClick={buttonClickHandler}>
-        +
+      <button name="1" type="button" onClick={e => buttonClickHandler(e.target.name)}>
+        1
+      </button>
+      <button name="2" type="button" onClick={e => buttonClickHandler(e.target.name)}>
+        2
+      </button>
+      <button name="3" type="button" onClick={e => buttonClickHandler(e.target.name)}>
+        3
+      </button>
+      <button name="4" type="button" onClick={e => buttonClickHandler(e.target.name)}>
+        4
       </button>
     </CalculatorContainer>
   )
