9f7ff76ef1e1c06f22b083cc7f9bbddcb1e813f5
taeeun kim <kimtaeeeny@gmail.com>
2020-03-01 19:33:37 +0900

    add note text in header

--- components/Header.js
+++ components/Header.js
@@ -2,17 +2,22 @@ import React, { useEffect, useState } from 'react'
 import styled from 'styled-components'
 
 const HeaderContainer = styled.div`
+  display: flex;
   position: fixed;
   top: 0;
   margin-top: 10px;
   z-index: 5;
+  font-size: 12px;
+  font-family: escore6;
+  color: #666;
 `
 
 const FullscreenText = styled.p`
-  font-size: 12px;
   cursor: pointer;
-  font-family: escore6;
-  color: #666;
+`
+
+const NoteText = styled.p`
+  cursor: pointer;
 `
 
 const Header = () => {
@@ -37,6 +42,7 @@ const Header = () => {
       <FullscreenText onClick={openFullScreen}>
         {full ? 'exit full screen' : 'full screen'}
       </FullscreenText>
+      &nbsp;&nbsp;|&nbsp;&nbsp;<NoteText> note</NoteText>
     </HeaderContainer>
   )
 }
