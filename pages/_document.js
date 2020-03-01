687f3ca2f533bab20ef338332aa3e792b60988e3
taeeun kim <kimtaeeeny@gmail.com>
2020-03-01 18:13:37 +0900

    :apple: next + styled component + babelrc

--- pages/_document.js
+++ pages/_document.js
@@ -3,16 +3,27 @@ import { ServerStyleSheet } from 'styled-components'
 import Document, { Html, Head, Main, NextScript } from 'next/document'
 
 class _document extends Document {
-  static getInitialProps(context) {
+  static getInitialProps({ renderPage }) {
+    // Step 1: Create an instance of ServerStyleSheet
     const sheet = new ServerStyleSheet()
-    const page = context.renderPage(App => props => sheet.collectStyles(<App {...props} />))
+
+    // Step 2: Retrieve styles from components in the page
+    const page = renderPage(App => props => sheet.collectStyles(<App {...props} />))
+
+    // Step 3: Extract the styles as <style> tags
     const styleTags = sheet.getStyleElement()
+
+    // Step 4: Pass styleTags as a prop
     return { ...page, styleTags }
   }
   render() {
     return (
       <Html>
-        <Head />
+        <Head>
+          <title>소마법프로젝트</title>
+          {/* Step 5: Output the styles in the head  */}
+          {this.props.styleTags}
+        </Head>
         <body style={{ fontFamily: 'escore3' }}>
           <Main />
           <NextScript />
