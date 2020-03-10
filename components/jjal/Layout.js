4e595285bd3b7f1f9470105b8d80bac656ac1a94
taeeun kim <kimtaeeeny@gmail.com>
2020-03-10 20:44:34 +0900

    canvas + image upload

--- components/jjal/Layout.js
+++ components/jjal/Layout.js
@@ -1,6 +1,8 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import styled from 'styled-components'
 import ContentsMenubar from '../ContentsMenubar'
+
+const imagePaths = ['cancel.png']
 
 const BackgroundContainer = styled.div`
   display: flex;
@@ -13,10 +15,52 @@ const BackgroundContainer = styled.div`
   background: #f5f6f7;
 `
 
+const JJalContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  & > button {
+    border: 2px solid pink;
+    border-radius: 3px;
+  }
+`
+
+const JJalForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`
+
 const Layout = () => {
+  useEffect(() => {})
+
+  function handleImage(e) {
+    var canvas = document.getElementById('imageCanvas')
+    var ctx = canvas.getContext('2d')
+    var reader = new FileReader()
+    reader.onload = function(event) {
+      var img = new Image()
+      img.onload = function() {
+        canvas.width = img.width / 10
+        canvas.height = img.height / 10
+        ctx.drawImage(img, 0, 0)
+      }
+      img.src = event.target.result
+    }
+    reader.readAsDataURL(e.target.files[0])
+  }
+
   return (
     <BackgroundContainer>
-      <ContentsMenubar name="jjal" />짤
+      <ContentsMenubar name="jjal" />
+      <JJalContainer>
+        <JJalForm id="form" runat="server">
+          <label>Image File:</label>
+          <br />
+          <input type="file" id="imageLoader" name="imageLoader" onChange={handleImage} />
+          <canvas id="imageCanvas"></canvas>
+        </JJalForm>
+      </JJalContainer>
     </BackgroundContainer>
   )
 }
