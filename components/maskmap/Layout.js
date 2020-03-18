279d2ba9f3061f8af7d0900c6d29c86b03fc1576
taeeun kim <kimtaeeeny@gmail.com>
2020-03-18 17:28:34 +0900

    update map event listener

--- components/maskmap/Layout.js
+++ components/maskmap/Layout.js
@@ -68,7 +68,7 @@ const Layout = () => {
       mapTypeId: naver.maps.MapTypeId.NORMAL
     })
 
-    naver.maps.Event.addListener(map, 'mousedown', function(e) {
+    naver.maps.Event.addListener(map, 'dragend', function(e) {
       const url = 'https://8oi9s0nnth.apigw.ntruss.com/corona19-masks/v1/storesByGeo/json'
       const query = `?lat=${map.getCenter().y}&lng=${map.getCenter().x}`
       fetch(url + query)
