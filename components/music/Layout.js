8e5cede3f5438f7fe7d30e672c1cd7b616e7417f
taeeun kim <kimtaeeeny@gmail.com>
2020-04-14 16:34:31 +0900

    :rocket: complete music app

--- components/music/Layout.js
+++ components/music/Layout.js
@@ -1,9 +1,15 @@
 import React, { useEffect, useState } from 'react'
-import styled from 'styled-components'
+import styled, { keyframes } from 'styled-components'
 import ContentsMenubar from '../ContentsMenubar'
 import Sound from './Sound'
 
 const musics = ['1Upbeat Ukulele.mp3', '2Instrumental.mp3', '3Inspiration.mp3']
+
+const rainbow = keyframes`
+0%{background-position:0% 82%}
+50%{background-position:100% 19%}
+100%{background-position:0% 82%}
+`
 
 const BackgroundContainer = styled.div`
   position: relative;
@@ -12,7 +18,13 @@ const BackgroundContainer = styled.div`
   align-items: center;
   width: 100vw;
   height: 100vh;
-  background: white;
+  background: ${(props) => {
+    return props.playToggle
+      ? 'linear-gradient(124deg,#ff2400,#e81d1d,#e8b71d,#e3e81d,#1de840,#1ddde8,#2b1de8,#dd00f3,#dd00f3);'
+      : 'white'
+  }}
+  background-size: 1800% 1800%;
+  animation: ${rainbow} 18s ease infinite;
 `
 
 const PlayButton = styled.div`
@@ -85,8 +97,9 @@ const Layout = () => {
   }
 
   return (
-    <BackgroundContainer>
+    <BackgroundContainer className="musicBackground" playToggle={playToggle}>
       <ContentsMenubar name="music" />
+      <audio preload="auto" controls="none" style={{ display: 'none' }}></audio>
       <SideButton position="left" onClick={() => anotherMusicPlay('left')} />
       <PlayButton onClick={playMusic}>
         <div>{myMusic.name}</div>
--- components/music/Sound.js
+++ components/music/Sound.js
@@ -2,13 +2,9 @@ export default class Sound {
   sound
   name
   constructor(src) {
-    this.sound = document.createElement('audio')
+    this.sound = document.querySelector('audio')
     this.name = src
     this.sound.src = src
-    this.sound.setAttribute('preload', 'auto')
-    this.sound.setAttribute('controls', 'none')
-    this.sound.style.display = 'none'
-    document.body.appendChild(this.sound)
   }
   play() {
     this.sound.play()
