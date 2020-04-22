c8a023efa1b9a6f0f87f359262c5262289802ecc
taeeun kim <kimtaeeeny@gmail.com>
2020-04-22 18:27:02 +0900

    deploy test

--- pages/_document.js
+++ pages/_document.js
@@ -3,12 +3,12 @@ import { ServerStyleSheet } from 'styled-components'
 import Document, { Html, Head, Main, NextScript } from 'next/document'
 
 class _document extends Document {
-  static getInitialProps({ renderPage }) {
-    const sheet = new ServerStyleSheet()
-    const page = renderPage(App => props => sheet.collectStyles(<App {...props} />))
-    const styleTags = sheet.getStyleElement()
-    return { ...page, styleTags }
-  }
+  // static getInitialProps({ renderPage }) {
+  //   const sheet = new ServerStyleSheet()
+  //   const page = renderPage(App => props => sheet.collectStyles(<App {...props} />))
+  //   const styleTags = sheet.getStyleElement()
+  //   return { ...page, styleTags }
+  // }
   render() {
     return (
       <Html>
---
+++ pages/test.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const test = () => {
+  return <div></div>
+}
+
+export default test
